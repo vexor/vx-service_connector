2c99c7edbd0b05236759aa5ebbf006af38cc5e50
Dmitry Galinsky <dima.exe@gmail.com>
2014-01-31 16:20:07 +0400

    add instrumentations directly to faraday, fixed faraday version, add activesupport to dependencies

--- Gemfile
+++ Gemfile
@@ -2,7 +2,3 @@ source 'https://rubygems.org'
 
 # Specify your gem's dependencies in vx-service_connector.gemspec
 gemspec
-
-group :development do
-  gem 'httplog'
-end
--- lib/vx/service_connector/instrumentation.rb
+++ lib/vx/service_connector/instrumentation.rb
@@ -1,29 +1,12 @@
 require 'active_support/notifications'
-
-module Vx
-  module ServiceConnector
-    class Instrumentation < Faraday::Middleware
-
-      def initialize(app, options = {})
-        super(app)
-        @name = options.fetch(:name, 'request.http')
-      end
-
-      def call(env)
-        trace_env = {
-          method:           env[:method],
-          url:              env[:url].to_s,
-          status:           env[:status],
-          response_headers: env[:response_headers].map{|k,v| "#{k}: #{v}" }.join("\n")
-        }
-        puts "ENV: #{env.inspect}"
-        ActiveSupport::Notifications.instrument(@name, trace_env) do
-          @app.call(env)
-        end
-      end
-    end
-  end
-end
-
+require 'faraday'
 require 'octokit/default'
-Octokit::Default::MIDDLEWARE.use Vx::ServiceConnector::Instrumentation
+
+builder = Faraday::RackBuilder.new
+builder.request :instrumentation
+
+Octokit::Default::MIDDLEWARE.request :instrumentation
+
+Faraday.default_connection_options = {
+  builder: builder
+}
